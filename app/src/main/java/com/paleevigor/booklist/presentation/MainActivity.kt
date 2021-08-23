867a72e3af531d9a92c0b20fa60db567d4a543e9
21paleev <paleev2121@gmail.com>
2021-08-23 21:57:10 +0300

    Creating presentation_1

--- app/src/main/java/com/paleevigor/booklist/presentation/MainActivity.kt
+++ app/src/main/java/com/paleevigor/booklist/presentation/MainActivity.kt
@@ -2,11 +2,23 @@ package com.paleevigor.booklist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.paleevigor.booklist.R
 
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        //подписываемся на объект bookList
+        viewModel.bookList.observe(this) {
+            Log.d("MainActivityTest", it.toString())
+        }
+        viewModel.getBookList()
     }
 }
